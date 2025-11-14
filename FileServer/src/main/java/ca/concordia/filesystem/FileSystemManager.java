931b4ce2114afb0995621ee3c87d898a13421610
LNN10 <nkrumahleugoue2005@gmail.com>
2025-11-14 01:11:03 -0500

    Implemented thread-safe file system operations in FileSystemManager

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 import java.io.RandomAccessFile;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class FileSystemManager {
 
@@ -16,6 +17,7 @@ public class FileSystemManager {
     private final static FileSystemManager instance;
     private final RandomAccessFile disk;
     private final ReentrantLock globalLock = new ReentrantLock();
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock(); // NEW lock for read/write/list
 
     private static final int BLOCK_SIZE = 128; // Example block size
 
@@ -169,7 +171,7 @@ public class FileSystemManager {
 
         // Write data to file
     public void writeFile(String fileName, byte[] data) throws Exception {
-        globalLock.lock();
+        rwLock.writeLock().lock();
         try {
             int idx = findFileIndex(fileName);
             if (idx == -1) {
@@ -224,13 +226,13 @@ public class FileSystemManager {
             System.out.println("Wrote " + data.length + " bytes to " + fileName);
 
         } finally {
-            globalLock.unlock();
+            rwLock.writeLock().unlock();
         }
     }
 
     // Read file data
     public byte[] readFile(String fileName) throws Exception {
-        globalLock.lock();
+        rwLock.readLock().lock();
         try {
             int idx = findFileIndex(fileName);
             if (idx == -1) {
@@ -266,13 +268,13 @@ public class FileSystemManager {
             return result;
 
         } finally {
-            globalLock.unlock();
+            rwLock.readLock().unlock();
         }
     }
 
     // list files
     public String[] listFiles() {
-        globalLock.lock();
+        rwLock.readLock().lock();
         try {
             int count = 0;
             for (FEntry entry : inodeTable) {
@@ -291,11 +293,9 @@ public class FileSystemManager {
 
             return names;
         } finally {
-            globalLock.unlock();
+            rwLock.readLock().unlock();
         }
     }
-
-
 
 
 
